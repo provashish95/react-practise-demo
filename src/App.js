12dbfc15b4544e0feb84b6bb7b76ec85612fca46
Provashish Roy <provashishroy95@gmail.com>
2022-03-24 16:06:08 +0600

    test componenet added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <div className="App">
       <Clock local="bn-BD">Present Time</Clock>
+      <Test></Test>
     </div>
   );
+}
+
+
+function Test() {
+  return (
+    <div>
+      <h1 style={{ color: 'red', backgroundColor: 'yellow' }}>This is test component</h1>
+    </div>
+  )
 }
 export default App;
