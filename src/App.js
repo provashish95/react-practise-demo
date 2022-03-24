52677726d02394c9f9b4baa833f8c5340e8a19c2
Provashish Roy <provashishroy95@gmail.com>
2022-03-24 14:18:10 +0600

    added extra components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import Clock from './components/Clock/Clock';
 
 function App() {
   return (
     <div className="App">
-      <Clock local="bn-BD"></Clock>
-      <Watch name="Provahish Roy"></Watch>
+      <Clock></Clock>
     </div>
   );
 }
-
-function Clock(props) {
-  const { local } = props;
-  return (
-    <h1 className="heading">
-      <span>Hello: {new Date().toLocaleDateString(local)}</span>
-    </h1>
-  );
-}
-
-//class component of react
-class Watch extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>This is class Component</h2>
-        <p>{this.props.name}</p>
-      </div>
-    );
-  }
-}
-
 export default App;
---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <h1>This is separate class component</h1>
+        );
+    }
+}
+export default Clock;
